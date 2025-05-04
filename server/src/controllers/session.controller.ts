13cc275929bfa6b217286493e5ecb2ea045aa47f
김용민 <dydals3440@gmail.com>
2025-05-05 04:10:17 +0900

    feat: 서버 구조 변경 및 모든 파일 이동

--- server/src/controllers/session.controller.ts
+++ server/src/controllers/session.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import SessionModel from '../models/session.model';
-import { OK } from '../constants/http';
+import { NOT_FOUND, OK } from '../constants/http';
+import { z } from 'zod';
+import appAssert from '../utils/appAssert';
 
 export const getSessionHandler = async (req: Request, res: Response) => {
   const sessions = await SessionModel.find(
@@ -29,4 +31,17 @@ export const getSessionHandler = async (req: Request, res: Response) => {
       }),
     }))
   );
+};
+
+export const deleteSessionHandler = async (req: Request, res: Response) => {
+  const sessionId = z.string().parse(req.params.id);
+  // 유저를 체크 안하면, 어떤 유저든 세션을 삭제 가능해짐
+  const deleted = await SessionModel.findOneAndDelete({
+    _id: sessionId,
+    userId: req.userId,
+  });
+  appAssert(deleted, NOT_FOUND, 'Session not found');
+  res.status(OK).json({
+    message: 'Session deleted successfully',
+  });
 };
--- server/src/routes/session.route.ts
+++ server/src/routes/session.route.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { getSessionHandler } from '../controllers/session.controller';
+import {
+  deleteSessionHandler,
+  getSessionHandler,
+} from '../controllers/session.controller';
 const sessionRoutes = Router();
 
 // prefix: /sessions
 sessionRoutes.get('/', getSessionHandler);
+sessionRoutes.delete('/:id', deleteSessionHandler);
 
 export default sessionRoutes;
